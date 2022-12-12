f8de519c56660a598a704da1725840c2318733f0
Ramesh Pradhan <ramesh.pradhan@chuchuro.com>
2022-12-12 14:21:58 +0545

    fix(blog-bootstrap-v5): :bug: add  LoginRequiredMixin in views

--- personal_blog/views.py
+++ personal_blog/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.utils import timezone
-from django.views.generic import ListView, DetailView, CreateView, View, UpdateView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, View
 
 from personal_blog.forms import PostForm
 from personal_blog.models import Post
@@ -23,14 +23,14 @@ class PostDetailView(DetailView):
     context_object_name = "post"
 
 
-class DraftListView(ListView):
+class DraftListView(LoginRequiredMixin, ListView):
     model = Post
     template_name = "post_list.html"
     context_object_name = "posts"
     queryset = Post.objects.filter(published_at__isnull=True).order_by("-published_at")
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     form_class = PostForm
     template_name = "post_create.html"
     success_url = reverse_lazy("drafts")
@@ -47,14 +47,14 @@ class PostDeleteView(View):
         return redirect("post-list")
 
 
-# class PostUpdateView(UpdateView):
+# class PostUpdateView(LoginRequiredMixin, UpdateView):
 #     model = Post
 #     form_class = PostForm
 #     template_name = "post_create.html"
 #     success_url = reverse_lazy("post-list")
 
 
-class PostPublishView(View):
+class PostPublishView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
         post.published_at = timezone.now()
@@ -62,7 +62,7 @@ class PostPublishView(View):
         return redirect("post-list")
 
 
-class PostUpdateView(View):
+class PostUpdateView(LoginRequiredMixin, View):
     def get(self, request, pk, *args, **kwargs):
         post = get_object_or_404(Post, pk=pk)
         form = PostForm(instance=post)
